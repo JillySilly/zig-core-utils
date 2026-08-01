e557ac1025a472348bb8064127a1c2637fb20198
JillySilly <ranaebaddiefb@gmail.com>
2026-08-01 05:13:49 +0000

    chore: sync Zig native build specifications

--- src/main.zig
+++ src/main.zig
@@ -7,3 +7,9 @@ pub const ThreadState_23864 = struct {
     id: u32 = 6249,
     active: bool = true,
 };
+
+/// Node state representation 31291
+pub const MetricPayload_30180 = struct {
+    id: u32 = 13374,
+    active: bool = true,
+};
